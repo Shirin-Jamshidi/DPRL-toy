065e7a8315cdecd893f34402e562f758be47473d
Shirin Jamshidi <shirinjamshidi2001@gmail.com>
2026-06-08 12:47:33 -0400

    Update

--- models.py
+++ models.py
@@ -1,33 +1,33 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 class QNetwork(nn.Module):
-    def __init__(self):
+    def __init__(self, input_dim):
+        super(QNetwork, self).__init__()
+
+        self.fc1 = nn.Linear(input_dim, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 1)
+
+    def forward(self, s, a):
+        x = torch.cat([s, a], dim=-1)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        return self.fc3(x)
+    
+class DiffusionPolicy(nn.Module):
+    def __init__(self, input_dim):
         super().__init__()
+
         self.net = nn.Sequential(
-            nn.Linear(3 + 1, 128),
+            nn.Linear(input_dim, 128),
             nn.ReLU(),
             nn.Linear(128, 128),
             nn.ReLU(),
             nn.Linear(128, 1)
         )
 
-    def forward(self, s, a):
-        x = torch.cat([s, a], dim=-1)
-        return self.net(x)
-    
-
-class DiffusionPolicy(nn.Module):
-    def __init__(self):
-        super().__init__()
-        self.net = nn.Sequential(
-            nn.Linear(3 + 1 + 1, 128),  # state + noisy_action + t
-            nn.ReLU(),
-            nn.Linear(128, 128),
-            nn.ReLU(),
-            nn.Linear(128, 1)  # predict noise
-        )
-
-    def forward(self, s, noisy_a, t):
-        x = torch.cat([s, noisy_a, t], dim=-1)
+    def forward(self, s, a, t):
+        x = torch.cat([s, a, t], dim=-1)
         return self.net(x)
